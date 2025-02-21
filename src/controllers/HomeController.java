5fd94ec738d262a9134edcb471c431ffbf49fb48
Nick <email@example.com>
2025-02-20 21:38:24 -0500

    Basic controller structure

--- src/controllers/HomeController.java
+++ src/controllers/HomeController.java
@@ -1,6 +1,19 @@
 package controllers;
 
-public class HomeController 
-{
+import views.HomeView;
 
+public class HomeController
+{
+    private HomeModel model;
+    private HomeView view;
+    private int id;
+
+    public HomeController(HomeModel model, HomeView view, int id)
+    {
+        this.model = model;
+        this.view = view;
+        this.id = id;
+
+        this.view.createView();
+    }
 }
--- src/controllers/LogInController.java
+++ src/controllers/LogInController.java
@@ -1,6 +1,40 @@
 package controllers;
 
-public class LogInController 
-{
+import models.AccountModel;
+import views.LogInView;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JOptionPane;
 
+public class LogInController
+{
+    private LogInView view;
+    private AccountModel model;
+
+    public LogInController(LogInView view, AccountModel model) 
+    {
+        this.view = view;
+        this.model = model;
+
+        this.view.addLogInListener(new AuthListener());
+    }
+
+    class AuthListener implements ActionListener {
+        
+        public void actionPerformed(ActionEvent e) { 
+            String username = view.getUsername();
+            String password = view.getPassword();
+
+            if (model.authenticate(username, password))
+            {
+                HomeController homeController = new HomeController(view, model, model.getID(username, password));
+            }
+            else
+            {
+                view.badLogin();
+                view.destroyView();
+                view.createView();
+            }
+        }
+    }
 }
